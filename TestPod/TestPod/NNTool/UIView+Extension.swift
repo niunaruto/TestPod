edf7e7e2ac40ef05808f51c964697d4c3871ed53
niuNaruto <niuting604@163.com>
2017-03-27 10:43:22 +0800

    Pod commint

---
+++ TestPod/TestPod/NNTool/UIView+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Extension.swift
+//  TestPod
+//
+//  Created by niuting on 2017/3/27.
+//  Copyright © 2017年 niuNaruto. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    /// The screen size
+    class var size: CGSize {
+        return UIScreen.main.bounds.size
+    }
+    
+    /// The screen's width
+    class var screenWidth: CGFloat {
+        return UIView.size.width
+    }
+    
+    /// The screen's height
+    class var screenHeight: CGFloat {
+        return UIView.size.height
+    }
+    
+    
+}
