4f8c292328129c24c8823dcc571262c099c59fdb
niuNaruto <niuting604@163.com>
2017-03-29 09:28:53 +0800

    Init

---
+++ TestPod/TestPod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPod
+//
+//  Created by niuting on 2017/3/29.
+//  Copyright © 2017年 niuNaruto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestPod/TestPod/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestPod
+//
+//  Created by niuting on 2017/3/29.
+//  Copyright © 2017年 niuNaruto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
